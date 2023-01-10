14ecc782ffe6d7de9ebfeb87d7e1aaa205f9ad68
Rubeno24 <ruben000_2@live.com>
2023-01-10 12:03:24 -0800

    change

--- main.js
+++ main.js
@@ -14,37 +14,37 @@ document.getElementById("Button").onclick = function(){
     if(percent=="100%"){
         document.getElementById("BlankLabel").innerHTML = "PERFECT SCORE";
     }
-    else if(percent<="99%"&&percent>"97%"){
+    else if(percent<="99%"&&percent>="97%"){
         document.getElementById("BlankLabel").innerHTML = percent+" A+";
     }
-    else if(percent<="96%"&&percent>"93%"){
+    else if(percent<="96%"&&percent>="93%"){
         document.getElementById("BlankLabel").innerHTML = percent+" A";
     }
-    else if(percent<="92%"&&percent>"90%"){
+    else if(percent<="92%"&&percent>="90%"){
         document.getElementById("BlankLabel").innerHTML = percent+" A-";
     }
-    else if(percent<="89%"&&percent>"87%"){
+    else if(percent<="89%"&&percent>="87%"){
         document.getElementById("BlankLabel").innerHTML = percent+" B+";
     }
-    else if(percent<="86%"&&percent>"83%"){
+    else if(percent<="86%"&&percent>="83%"){
         document.getElementById("BlankLabel").innerHTML = percent+" B";
     }
-    else if(percent<="82%"&&percent>"80%"){
+    else if(percent<="82%"&&percent>="80%"){
         document.getElementById("BlankLabel").innerHTML = percent+" B-";
     }
-    else if(percent<="79%"&&percent>"77%"){
+    else if(percent<="79%"&&percent>="77%"){
         document.getElementById("BlankLabel").innerHTML = percent+" C+";
     }
-    else if(percent<="76%"&&percent>"73%"){
+    else if(percent<="76%"&&percent>="73%"){
         document.getElementById("BlankLabel").innerHTML = percent+" C";
     }
-    else if(percent<="72%"&&percent>"70%"){
+    else if(percent<="72%"&&percent>="70%"){
         document.getElementById("BlankLabel").innerHTML = percent+" C-";
     }
-    else if(percent<="69%"&&percent>"67%"){
+    else if(percent<="69%"&&percent>="67%"){
         document.getElementById("BlankLabel").innerHTML = percent+" D+";
     }
-    else if(percent<="66%"&&percent>"65%"){
+    else if(percent<="66%"&&percent>="65%"){
         document.getElementById("BlankLabel").innerHTML = percent+" D";
     }
     else 
